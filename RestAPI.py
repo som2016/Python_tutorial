da40f71d2c5bc75cdd360c77411f09e315ff8797
som2016 <somarko1989@hotmail.com>
2019-11-30 11:26:53 +0530

    Python PSec

--- RestAPI.py
+++ RestAPI.py
@@ -1,8 +1,15 @@
 import requests
 import json
 from requests.auth import HTTPBasicAuth
+import re
+#from getpass import getpass            ## to take passwords without displaying it
+list1 = list()
+fp = open('Passwords.txt', 'r')
+for line in fp:
+    temp = re.sub('[\n: \t]', '', line)
+    list1.append(temp)
 
-#from getpass import getpass
+print('___________________________ __________________ ________________________________________________________________')
 print('___________________________ Basic POST Request ________________________________________________________________')
 print('___________________________ __________________ ________________________________________________________________')
 url = 'https://api.github.com/user/repos'
@@ -13,7 +20,7 @@ data = {
   "has_wiki": "true"
 }
 login = requests.session()
-login.auth= HTTPBasicAuth('som2016', 'Idea&1234')
+login.auth= HTTPBasicAuth(list1[0], list1[1])
 response = login.post(url,  verify=False, data=json.dumps(data))    ##Verify false as SSL Certificate check false else give the path
 #response.request.headers['Content-Type'] = 'application/json'      ## Without this its still working
 #print(response.request.header)
@@ -21,6 +28,8 @@ print(response.status_code)
 print(response.content)
 print(response.request.url)
 print(response.request.body)
+
+
 print('___________________________ __________________ ________________________________________________________________')
 print('___________________________ Basic GET Request ________________________________________________________________')
 print('___________________________ __________________ ________________________________________________________________')
@@ -32,6 +41,9 @@ temp = response.json()
 #print(json.dumps(temp, sort_keys=True, indent=4))         ## sort_keys = True tells the encoder to return the JSON object keys in a sorted order,
 with open('data.json', 'w') as f:                          ## Data.json is the new file name
     json.dump(temp, f, sort_keys=True, indent=4)           ## Note here json.dumps() won't work it will give blank file
+
+
+
 print('___________________________ __________________ ________________________________________________________________')
 print('___________________________ Basic DEL Request ________________________________________________________________')
 print('___________________________ __________________ ________________________________________________________________')
